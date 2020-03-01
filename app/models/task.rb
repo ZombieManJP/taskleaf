7f8a909a6d07fd9f0bfd7a563a9e8a90d1552368
zombiemanjp <f.d0525774@gmail.com>
2020-03-01 22:40:20 +0900

    メイラー機能追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,15 @@ class Task < ApplicationRecord
     validates :name, presence: true, length: {maximum:30}
     validate :validate_name_not_including_comma
 
+    def self.ransackable_attributes(auth_object = nil)
+        %w[name created_at]
+    end
+
+    def self.ransackable_associations(auth_object = nil)
+        []
+    end
+
+
     belongs_to :user
 
     scope :recent, -> { order(created_at: :desc) }
